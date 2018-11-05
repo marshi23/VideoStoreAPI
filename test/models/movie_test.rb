cb55128372991d917c2b6f4806e3f2c0ecaa0c7b
Maryam Shitu <maryamshitu@gmail.com>
2018-11-05 15:31:59 -0800

    movie tests added

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let (:movie) { movies (:rats) }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "can be created" do
+   expect(movie.valid?).must_equal true
+  end
+
+  it "requires title, overview, release_date and inventory" do
+    required_fields = [:title, :overview, :release_date, :inventory]
+
+    required_fields.each do |field|
+      movie[field] = nil
+
+      expect(movie.valid?).must_equal false
+
+      movie.reload
+    end
+  end
+
+  it "requires a numnerice inventory count" do
+    movie.inventory = "one"
+
+    expect(movie.valid?).must_equal false
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/rails"require "minitest/reporters"  # for Colorized output
+require "minitest/rails"
+require "minitest/reporters"  # for Colorized output
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -15,7 +16,7 @@ Minitest::Reporters.use!(
 # require "minitest/rails/capybara"
 
 # Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
