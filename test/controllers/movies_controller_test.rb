3133cea1adc9c58f9132dc869b8947438a0b0f19
Xtina <2006peacegypsy@gmail.com>
2018-11-06 14:29:51 -0800

    seven tests in movie controller, all passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -3,9 +3,29 @@ require "test_helper"
 describe MoviesController do
   describe 'index' do
     it 'succeeds' do
+      #Act
+      get movies_path
+      #Assert
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
+    it'returns an Array' do
       get movies_path
 
-      must_respond_with :success
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      # Act
+      get movies_path
+
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body.length).must_equal Movie.count
     end
   end
 
@@ -15,19 +35,32 @@ describe MoviesController do
 
       must_respond_with :success
     end
+
+    it 'can get a movie with a valid info' do
+      get movies_path(movies(:rats).id)
+      must_respond_with :success
+    end
+
+    it "responds with a not_found if no movie with that id exists" do
+      id = -1
+
+      get movie_path(id)
+
+      must_respond_with :not_found
+    end
   end
 
   describe 'create' do
     let(:movie_hash) do
-    {
-      movie: {
-        title: 'Eternal sunshine of the Spotless Mind',
-        overview: 'Kate Winslet and Jim Carrey',
-        release_date: '2012-11-25',
-        inventory: "3"
+      {
+        movie: {
+          title: 'Eternal sunshine of the Spotless Mind',
+          overview: 'Kate Winslet and Jim Carrey',
+          release_date: '2012-11-25',
+          inventory: "3"
+        }
       }
-    }
-  end
+    end
     it 'creates a movie given correct params' do
       expect {
         post movies_path, params: movie_hash
