853030b5f16deeb9edc3229fd7626c265bb1400d
Xtina <2006peacegypsy@gmail.com>
2018-11-05 16:39:37 -0800

    added show method, works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,29 @@
 class MoviesController < ApplicationController
+
+  def index
+    movies = Movie.all
+    render json:  movies.as_json(only: [:id, :title, :release_date]), status: :ok
+  end
+
+  def show
+  movie = Movie.find_by(id: params[:id])
+  render json:  movie.as_json(only: [:title, :review, :release_date, :inventory]), status: :ok
+  end
+
+  # def search(id)
+  #   movie = Movie.find_by(id: params[:movie_id])
+  # end
+
+  def create
+    movie = Movie.new(movie_params)
+    movie.save!
+    render json: { id: movie.id }, status: :ok
+  end
+end
+
+
+
+private
+def movie_params
+  params.require(:movie).permit(:title, :overview, :release_date, :inventory)
 end
