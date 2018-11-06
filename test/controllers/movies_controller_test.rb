394c72fe3e148f78780cdbb2fa9d4cf53c8a82de
Xtina <2006peacegypsy@gmail.com>
2018-11-05 21:22:27 -0800

    three controller tests, all passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -17,4 +17,24 @@ describe MoviesController do
     end
   end
 
+  describe 'create' do
+    let(:movie_hash) do
+    {
+      movie: {
+        title: 'Eternal sunshine of the Spotless Mind',
+        overview: 'Kate Winslet and Jim Carrey',
+        release_date: '2012-11-25',
+        inventory: "3"
+      }
+    }
+  end
+    it 'creates a movie given correct params' do
+      expect {
+        post movies_path, params: movie_hash
+      }.must_change 'Movie.count', 1
+
+      must_respond_with :ok
+    end
+
+  end
 end
