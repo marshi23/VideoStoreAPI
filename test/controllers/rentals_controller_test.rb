f1948c8805a3ec04c39675d39ac735deeddca9f2
Maryam Shitu <maryamshitu@gmail.com>
2018-11-09 12:36:33 -0800

    rental test added"

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -5,9 +5,10 @@ describe RentalsController do
 
     it "has a working route" do
       rental_id = 1
+
       rental_params = {
-        customer_id: 298486374, # id for first customer in customer yml
-        movie_id: 298486374 # id for first movie in movie yml
+        customer_id: customers(:one)[:id], # id for first customer in customer yml
+        movie_id: movies(:one)[:id] # id for first movie in movie yml
       }
 
       post checkout_path(rental_id), { :params => rental_params }
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -3,7 +3,11 @@ require "test_helper"
 describe Rental do
   let (:rental) { rentals (:one) }
 
-  it "requires customer_id, movie_id, and checkin_date" do
+  it "can be created" do
+      expect(rental.valid?).must_equal true
+  end
+
+  it "is invalid when customer_id, movie_id, and checkin_date are not provided" do
     required_fields = [:customer_id, :movie_id, :checkin_date]
 
     required_fields.each do |field|
@@ -14,4 +18,5 @@ describe Rental do
       rental.reload
     end
   end
+
 end
