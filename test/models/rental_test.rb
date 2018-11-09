7160f0d91c23a830ec712caad72aa494f692f018
Maryam Shitu <maryamshitu@gmail.com>
2018-11-09 13:12:53 -0800

    adding test

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -4,9 +4,13 @@ describe Rental do
   let (:rental) { rentals (:one) }
   let (:movie) { movies (:one)}
 
+  it "can be created" do
+      expect(rental.valid?).must_equal true
+  end
+
   it "will fail without customer_id, movie_id, and checkin_date" do
     required_fields = [:customer_id, :movie_id, :checkin_date]
-binding.pry
+# binding.pry
     required_fields.each do |field|
 
       rental[field] = nil
